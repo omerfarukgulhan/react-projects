cd40779e20e0d453e0d5b35326af84b13919436b
omerfarukgulhan <101866866+omerfarukgulhan@users.noreply.github.com>
2023-12-24 18:47:24 +0300

    tic tac toe added and folders edited

---
+++ tic-tac-toe/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <img src="src\images\game-logo.png" alt="Hand-drawn tic tac toe game board" />
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
